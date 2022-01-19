7a27c7a85cd60689dadee785b3d56a3faf0802e0
DiegoHumberoSalgado2000 <diegohumbertosalgadoramirez@gmail.com>
2022-01-19 01:21:55 -0500

    se agregaron las imagenes en el home y los botones de megusta y no me gusta

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,7 +6,8 @@ import {VengadoresComponent} from "./components/vengadores/vengadores.component"
 const APP_ROUTES: Routes=[
   {path: 'home', component: HomeComponent},
   {path: 'resultado', component: ResultadoComponent},
-  {path: 'vengadores', component: VengadoresComponent}
+  {path: 'vengadores', component: VengadoresComponent},
+  {path: '**', pathMatch: 'full', redirectTo: 'home'}
 ];
 
 export const APP_ROUTING=RouterModule.forRoot(APP_ROUTES);
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import {VengadoresService,Avengers} from "../../services/avengers.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+vengadores:Avengers[]=[];
+
+  constructor(private _avengerService:VengadoresService, private router:Router) { }
 
   ngOnInit(): void {
+this.vengadores=this._avengerService.getVideoavengers();
   }
 
+verVengador(idx: number){
+this.router.navigate(['/vengador/',idx]);
+}
 }
