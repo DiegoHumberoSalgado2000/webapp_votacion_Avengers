73babac9a9fa6e2e66a7feb7287eae2dacdb84c3
DiegoHumberoSalgado2000 <diegohumbertosalgadoramirez@gmail.com>
2022-01-18 19:45:01 -0500

    se agrego la barra de navegacion

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NabvarComponent } from './components/nabvar/nabvar.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NabvarComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule
